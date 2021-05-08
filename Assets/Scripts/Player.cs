fcae9728002bd954949061b8ff0beef18ff99fcb
Jelani Woods <jelani@firstdraft.com>
2021-05-08 16:21:57 -0500

    Start Jump logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,13 +9,23 @@ public class Player : MonoBehaviour
     public Vector2 direction;
     private bool facingRight = true;
 
+    [Header("Verical Movement")]
+    public float jumpSpeed = 15f;
+
     [Header("Components")]
     public Rigidbody2D rb;
     public Animator animator;
+    public LayerMask groundLayer;
 
     [Header("Physics")]
     public float maxSpeed = 7f;
     public float linearDrag = 4f;
+    public float gravity = 1;
+    public float fallMultiplier = 5f;
+
+    [Header("Collision")]
+    public bool onGround = false;
+    public float groundLength = 0.6f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +36,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        onGround = Physics2D.Raycast(transform.position, Vector2.down, groundLength, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            Jump();
+        }
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
@@ -54,16 +70,38 @@ public class Player : MonoBehaviour
         }
     }
 
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+    }
+
     void modifyPhysics()
     {
         bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
 
-        if(Mathf.Abs(direction.x) < 0.4f || changingDirections)
+        if(onGround)
         {
-            rb.drag = linearDrag;
+
+            if(Mathf.Abs(direction.x) < 0.4f || changingDirections)
+            {
+                rb.drag = linearDrag;
+            } else
+            {
+                rb.drag = 0;
+            }
+            rb.gravityScale = 0;
         } else
         {
-            rb.drag = 0;
+            rb.gravityScale = gravity;
+            rb.drag = linearDrag * 0.15f;
+            if(rb.velocity.y < 0)
+            {
+                rb.gravityScale = gravity * fallMultiplier;
+            } else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.gravityScale = gravity * (fallMultiplier / 2);
+            }
         }
     }
 
@@ -72,5 +110,11 @@ public class Player : MonoBehaviour
         facingRight = !facingRight;
 
         transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundLength);
     }
 }
